cd8c2144c5a5ee36a254c84627abfadf0c367533
Andy Bell <ambell@ucdavis.edu>
2016-06-20 16:12:18 -0400

    initial yo project setup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,62 @@
  * # MainCtrl
  * Controller of the ngApp
  */
-angular.module('ngApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+var app = angular.module('ngApp');
+
+app.controller('MainCtrl', function() {
+  this.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
+});
+
+
+app.controller('ProjectCtrl', function() {
+  this.sites = [];
+});
+
+
+app.controller('TableCtrl', function() {
+  // use this controller to automatically build the table == TODO
+  this.selectedRow = null;
+  this.selectedFeature = null;
+  this.setClickedRow = function(index, name) {
+    this.selectedRow = index;
+    this.selectedFeature = name;
+  };
+});
+
+app.controller('MapCtrl', function() {
+  this.test = 'I m a test controller';
+  this.again = 'redo';
+  this.center = {
+    lat: 38.21,
+    lng: -121.309,
+    zoom: 12
+  };
+  this.defaults = {
+    scrollWheelZoom: false
+  };
+  this.events = null;
+});
+
+app.controller('SiteCtrl', function($scope, $http) {
+    var self = this;
+    $http.get('map.geojson').then(function(response) {
+    var features = [];
+    for (var i = 0; i < response.data.features.length; i++) {
+      var feat = response.data.features[i];
+      features.push(feat);
+    }
+    self.sites = features;
+    self.geojson = {
+      data: response.data
+    };
   });
+
+  $scope.search = {
+      name: '',
+      variable: '',
+  };
+});
